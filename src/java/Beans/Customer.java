b693c092c974a3b9957a6f4e788ee6e5cde9b4eb
aboza <lexboza@gmail.com>
2015-06-07 14:41:08 -0600

    Tareas ejecutadas: -Scripts de Creacion de Bases de datos

--- src/java/Beans/Customer.java
+++ src/java/Beans/Customer.java
@@ -11,11 +11,7 @@ package Beans;
  */
 public class Customer {
     private String name;
-    private String type;
     private String companyName;
-    private String firstName;
-    private String lastName;
-    private String contact;
     private String phone;
     private String fax;
     private String email;
@@ -34,44 +30,12 @@ public class Customer {
         this.name = name;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getCompanyName() {
         return companyName;
     }
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
     }
 
     public String getPhone() {
